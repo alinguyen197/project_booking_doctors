50506c838245499c24eb7a9bbdf48c6850388157
alinguyen197 <alinguyen197@gmail.com>
2025-10-02 22:27:40 +0700

    Implement API Delete and Create #39

--- BE/src/routes/userRouter.js
+++ BE/src/routes/userRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
-import upload from '../middleware/fileUpload'
+import upload from "../middleware/fileUpload";
 const router = express.Router();
 
-import userController from "../controllers/userController"
+import userController from "../controllers/userController";
 
 // login
 router.post("/login", userController.handleLogin);
 
-router.post("/get-all-users", userController.handleGetAllUser)
+router.post("/get-all-users", userController.handleGetAllUser);
+router.post("/create-new-user", userController.handleCreateNewUser);
+router.post("/delete-user", userController.handleDeleteUser);
 
 // Case upload file
 // router.post("/get-all-users", upload.single('file') , userController.handleGetAllUser)
